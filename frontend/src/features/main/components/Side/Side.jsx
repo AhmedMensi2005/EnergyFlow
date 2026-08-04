b168c396f1c9f7b3d7267827ee402f9cfa51031e
AhmedMensi2005 <amensi705@gmail.com>
2026-08-04 11:02:42 +0100

    side fixe

--- frontend/src/features/main/components/Side/Side.jsx
+++ frontend/src/features/main/components/Side/Side.jsx
@@ -3,33 +3,11 @@ import SidebarHeader from "./SidebarHeader";
 import SidebarNav from "./SidebarNav";
 import SidebarFooter from "./SidebarFooter";
 
-<<<<<<< HEAD
-export default function Side({ activePage, setActivePage }) {
 
-    const user = JSON.parse(
-        localStorage.getItem("user")
-=======
+
+    
 export default function Side({activePage,setActivePage,onExport}){
-
-    
-
-    return (
-    <Sidebar
-      header={<SidebarHeader subTitle="Startup Village" />}
-      nav={<SidebarNav activePage={activePage} setActivePage={setActivePage} onExport={onExport}/>}
-      footer={
-        <SidebarFooter
-            username="Name"
-            role="Role"
-            avatar="https://i.pravatar.cc/40"
-        />
-      }
-    />
-
-    
->>>>>>> origin/feature/devices
-    );
-
+    const user = JSON.parse(localStorage.getItem("user"));
     const initials = user?.username
     ? user.username
           .split(" ")
@@ -38,34 +16,16 @@ export default function Side({activePage,setActivePage,onExport}){
           .substring(0, 2)
           .toUpperCase()
     : "?";
+    
 
     return (
-
-        <Sidebar
-
-            header={
-                <SidebarHeader
-                    subTitle="Startup Village"
-                />
-            }
-
-            nav={
-                <SidebarNav
-                    activePage={activePage}
-                    setActivePage={setActivePage}
-                />
-            }
-
-            footer={
-                <SidebarFooter
-                    username={user?.username || "Unknown"}
-                    role={user?.role || ""}
-                    avatar={initials}
-                />
-            }
-
-        />
-
+    <Sidebar
+      header={<SidebarHeader subTitle="Startup Village" />}
+      nav={<SidebarNav activePage={activePage} setActivePage={setActivePage} onExport={onExport}/>}
+      footer={<SidebarFooter username={user?.username || "Unknown"} role={user?.role || ""} avatar={initials}/>}
+    />
     );
+
+    
 
 }
